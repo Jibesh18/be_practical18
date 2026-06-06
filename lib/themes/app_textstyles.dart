205baea2982a7853c0a997a309063ffd372d6f77
Jibesh16 <jibeshshresthaths@gmail.com>
2026-06-07 01:11:30 +0545

    Modifying textsyles

--- lib/themes/app_textstyles.dart
+++ lib/themes/app_textstyles.dart
@@ -2,31 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTextStyles {
+
+  // Display / Hero Text
   static TextStyle displayLarge = GoogleFonts.inter(
     fontSize: 32,
     fontWeight: FontWeight.w800,
-    letterSpacing: -0.8,
+    letterSpacing: -0.5,
     height: 1.1,
   );
 
+  // Authentication Titles
+  static TextStyle authTitle = GoogleFonts.inter(
+    fontSize: 34,
+    fontWeight: FontWeight.w800,
+    letterSpacing: -0.5,
+    height: 1.1,
+  );
+
+  static TextStyle authSubtitle = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    color: Colors.grey,
+  );
+
+  // Headlines
   static TextStyle headlineLarge = GoogleFonts.inter(
     fontSize: 28,
     fontWeight: FontWeight.w700,
-    letterSpacing: -0.6,
+    letterSpacing: -0.5,
     height: 1.15,
   );
 
   static TextStyle headlineMedium = GoogleFonts.inter(
     fontSize: 22,
     fontWeight: FontWeight.w700,
-    letterSpacing: -0.4,
+    letterSpacing: -0.3,
     height: 1.2,
   );
 
+  // Titles
   static TextStyle titleLarge = GoogleFonts.inter(
     fontSize: 18,
     fontWeight: FontWeight.w600,
-    letterSpacing: -0.2,
+    letterSpacing: -0.1,
     height: 1.25,
   );
 
@@ -36,6 +55,7 @@ class AppTextStyles {
     height: 1.3,
   );
 
+  // Body
   static TextStyle bodyLarge = GoogleFonts.inter(
     fontSize: 16,
     fontWeight: FontWeight.w400,
@@ -54,12 +74,14 @@ class AppTextStyles {
     height: 1.4,
   );
 
+  // Caption
   static TextStyle caption = GoogleFonts.inter(
     fontSize: 12,
     fontWeight: FontWeight.w400,
     height: 1.4,
   );
 
+  // Labels
   static TextStyle labelLarge = GoogleFonts.inter(
     fontSize: 14,
     fontWeight: FontWeight.w600,
@@ -78,9 +100,10 @@ class AppTextStyles {
     letterSpacing: 0.1,
   );
 
+  // Buttons
   static TextStyle button = GoogleFonts.inter(
     fontSize: 16,
-    fontWeight: FontWeight.w600,
-    letterSpacing: 0.2,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.1,
   );
 }
