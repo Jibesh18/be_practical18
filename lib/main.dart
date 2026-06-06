d8a354e903aefe8f145df014b8b162620b9bf09e
Jibesh16 <jibeshshresthaths@gmail.com>
2026-06-07 01:02:21 +0545

    Adding main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 import 'firebase_options.dart';
@@ -10,9 +11,22 @@ import 'themes/app_themes.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Pre-load Inter font so it doesn't fetch from network during animations,
+  // which is the primary cause of frame drops on splash / login screens.
+  await GoogleFonts.pendingFonts([
+    GoogleFonts.inter(),
+    GoogleFonts.inter(fontWeight: FontWeight.w400),
+    GoogleFonts.inter(fontWeight: FontWeight.w500),
+    GoogleFonts.inter(fontWeight: FontWeight.w600),
+    GoogleFonts.inter(fontWeight: FontWeight.w700),
+    GoogleFonts.inter(fontWeight: FontWeight.w800),
+  ]);
+
   runApp(const BePracticalApp());
 }
 
