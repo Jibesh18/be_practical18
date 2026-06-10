1e4f67ac0c4b4033a576aa1ac7a3a8a6eaa2c8c8
Sagar Adhiakri <sagardanuwar30@gmail.com>
2026-06-10 22:16:59 +0545

    loading widget

---
+++ lib/components/common/loading_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../../theme/app_colors.dart';
+import '../../theme/app_text_styles.dart';
+import '../../theme/spacing.dart';
+
+class LoadingWidget extends StatelessWidget {
+  final String? message;
+
+  const LoadingWidget({Key? key, this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+      const CircularProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(AppColors.primary),
+    ),
