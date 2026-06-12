8365f5f87d1386fd8af0d18c2b45b328ae591551
Jibesh16 <jibeshshresthaths@gmail.com>
2026-06-12 12:27:21 +0545

    add EmployerHomeScreen with 4-tab navigation

--- lib/views/screens/employer_home_screen.dart
+++ lib/views/screens/employer_home_screen.dart
@@ -1,15 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../themes/app_colors.dart';
+import '../../viewmodels/employer_viewmodel.dart';
+import '../tabs/employers_listings_tab.dart';
+import '../tabs/employer_post_tab.dart';
+import '../tabs/employer_applicants_tab.dart';
+import '../tabs/employer_profile_tab.dart';
 
-class EmployerHomeScreen extends StatelessWidget {
+class EmployerHomeScreen extends StatefulWidget {
   const EmployerHomeScreen({super.key});
 
   @override
+  State<EmployerHomeScreen> createState() => _EmployerHomeScreenState();
+}
+
+class _EmployerHomeScreenState extends State<EmployerHomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _tabs = const [
+    EmployerListingsTab(),
+    EmployerPostTab(),
+    EmployerApplicantsTab(),
+    EmployerProfileTab(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Employer Home Screen',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return ChangeNotifierProvider(
+      create: (_) => EmployerViewModel(),
+      child: Scaffold(
+        body: IndexedStack(
+          index: _currentIndex,
+          children: _tabs,
+        ),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _currentIndex,
+          onDestinationSelected: (i) => setState(() => _currentIndex = i),
+          backgroundColor: Theme.of(context).brightness == Brightness.dark
+              ? AppColors.darkSurface
+              : Colors.white,
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.list_alt_outlined),
+              selectedIcon: Icon(Icons.list_alt_rounded),
+              label: 'My Listings',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.add_circle_outline_rounded),
+              selectedIcon: Icon(Icons.add_circle_rounded),
+              label: 'Post Job',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.people_outline_rounded),
+              selectedIcon: Icon(Icons.people_rounded),
+              label: 'Applicants',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.person_outline_rounded),
+              selectedIcon: Icon(Icons.person_rounded),
+              label: 'Profile',
+            ),
+          ],
         ),
       ),
     );
