b590e53f4580030e36e13de67ba97025c5f2af31
Jibesh16 <jibeshshresthaths@gmail.com>
2026-06-12 12:29:37 +0545

    feat: add AppRoutes with named routes and transitions

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../views/screens/employer_home_screen.dart';
+import '../views/screens/intern_home_screen.dart';
 import '../views/screens/splash_screen.dart';
 import '../views/screens/login_screen.dart';
 import '../views/screens/register_screen.dart';
 import '../views/widgets/auth_gate.dart';
 
-class AppRoutes {
+
+  class AppRoutes {
   static const String splash = '/';
   static const String login = '/login';
   static const String register = '/register';
   static const String authGate = '/authGate';
+  static const String employerHome = '/employerHome';
+  static const String internHome = '/internHome';
 
   static Map<String, WidgetBuilder> get routes => {
-    splash: (context) => const SplashScreen(),
-    login: (context) => const LoginScreen(),
-    register: (context) => const RegisterScreen(),
-    authGate: (context) => const AuthGate(),
+  splash: (context) => const SplashScreen(),
+  login: (context) => const LoginScreen(),
+  register: (context) => const RegisterScreen(),
+  authGate: (context) => const AuthGate(),
+  employerHome: (context) => const EmployerHomeScreen(),
+  internHome: (context) => const InternHomeScreen(),
   };
+
 
   static Route<dynamic> smoothRoute(Widget page) {
     return PageRouteBuilder(
