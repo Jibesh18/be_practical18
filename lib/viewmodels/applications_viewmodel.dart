e6f508a18d877c25573956a0e8be2241383f8b7e
bipin-mysterio <forcollegepurpose1999@gmail.com>
2026-06-17 11:48:57 +0545

    feat: update ApplicationsViewModel

---
+++ lib/viewmodels/applications_viewmodel.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../models/application.dart';
+
+class ApplicationsViewModel extends ChangeNotifier {
+  final List<Application> _applications = [
+    Application(
+      title: 'Data Analyst Intern',
+      company: 'ABC Tech',
+      status: 'Under Review',
+      date: 'Applied 2 days ago',
+      color: Colors.orange,
+    ),
+    Application(
+      title: 'Flutter Developer',
+      company: 'TechNepal',
+      status: 'Interview Scheduled',
+      date: 'Applied 1 week ago',
+      color: Colors.blue,
+    ),
+    Application(
+      title: 'Marketing Intern',
+      company: 'GrowthHub',
+      status: 'Rejected',
+      date: 'Applied 3 weeks ago',
+      color: Colors.red,
+    ),
+  ];
+
+  List<Application> get applications => _applications;
+}
