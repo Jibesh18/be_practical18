a52ede2c33753a9c19e07d2035615571659502bc
bipin-mysterio <forcollegepurpose1999@gmail.com>
2026-06-17 11:48:40 +0545

    feat: update CommunityPost model

---
+++ lib/models/community_post.dart
@@ -0,0 +1,11 @@
+class CommunityPost {
+  final String question;
+  final String author;
+  final String answers;
+
+  CommunityPost({
+    required this.question,
+    required this.author,
+    required this.answers,
+  });
+}
