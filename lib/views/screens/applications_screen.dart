e7892b36586157832338ec4378340ac55ea772a1
bipin-mysterio <forcollegepurpose1999@gmail.com>
2026-06-17 11:49:40 +0545

    feat: update ApplicationsScreen

---
+++ lib/views/screens/applications_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/application.dart';
+import '../../viewmodels/applications_viewmodel.dart';
+
+
+class ApplicationsScreen extends StatelessWidget {
+  const ApplicationsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Applications', style: TextStyle(fontWeight: FontWeight.bold)),
+        centerTitle: true,
+      ),
+      body: Consumer<ApplicationsViewModel>(
+        builder: (context, viewModel, child) {
+          return ListView.builder(
+            padding: const EdgeInsets.all(16),
+            itemCount: viewModel.applications.length,
+            itemBuilder: (context, index) {
+              final application = viewModel.applications[index];
+              return ApplicationCard(application: application);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ApplicationCard extends StatelessWidget {
+  final Application application;
+
+  const ApplicationCard({super.key, required this.application});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 12),
+      child: ListTile(
+        leading: Icon(Iconsax.document, color: application.color),
+        title: Text(application.title, style: const TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Text(application.company),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(application.status, style: TextStyle(color: application.color, fontWeight: FontWeight.w600)),
+            Text(application.date, style: const TextStyle(fontSize: 12, color: Colors.grey)),
+          ],
+        ),
+      ),
+    );
+  }
+}
