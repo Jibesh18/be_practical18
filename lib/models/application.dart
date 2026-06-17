73a56d75fdf48f3654579c02e44b9927e3b56f27
bipin-mysterio <forcollegepurpose1999@gmail.com>
2026-06-17 11:48:29 +0545

    feat: update Application model

---
+++ lib/models/application.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Application {
+  final String title;
+  final String company;
+  final String status;
+  final String date;
+  final Color color;
+
+  Application({
+    required this.title,
+    required this.company,
+    required this.status,
+    required this.date,
+    required this.color,
+  });
+}
