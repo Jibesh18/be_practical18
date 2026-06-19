d5a7c90aa92fd440c761b8fc9493266166de6aec
Jibesh16 <jibeshshresthaths@gmail.com>
2026-06-19 10:21:15 +0545

    feat: add CommunityViewModel from bipin

---
+++ lib/viewmodels/community_viewmodel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import '../models/community_post.dart';
+
+class CommunityViewModel extends ChangeNotifier {
+  final List<CommunityPost> _posts = [
+    CommunityPost(question: 'How to prepare for Data Analyst interviews?', author: 'Sarah K.', answers: '24 answers'),
+    CommunityPost(question: 'Anyone got selected in TechNepal Flutter Internship?', author: 'Rohan P.', answers: '12 answers'),
+    CommunityPost(question: 'Best resources to learn Power BI?', author: 'Anita Sharma', answers: '8 answers'),
+  ];
+
+  List<CommunityPost> get posts => _posts;
+}
