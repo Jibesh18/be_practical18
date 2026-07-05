d0e7b54894d3d72fca1c81bb3993643204207c7a
Anish-to <gautamanish1111111@gmail.com>
2026-07-05 11:29:00 +0545

    Update profile_provider.dart

--- lib/viewmodels/profile_provider.dart
+++ lib/viewmodels/profile_provider.dart
@@ -1,8 +1,1 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../model/user.dart';
-import 'auth_provider.dart';
 
-final profileProvider = FutureProvider<User?>((ref) async {
-  final authState = ref.watch(authProvider);
-  return authState.user;
-});
