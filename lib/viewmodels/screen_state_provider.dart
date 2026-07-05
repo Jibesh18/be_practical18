03905e69e109ca1444460f64014030dd963ea40a
Anish-to <gautamanish1111111@gmail.com>
2026-07-05 11:29:32 +0545

    Update screen_state_provider.dart

--- lib/viewmodels/screen_state_provider.dart
+++ lib/viewmodels/screen_state_provider.dart
@@ -1,1 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum MainScreen {
+  home,
+  internships,
+  skills,
+  notifications,
+  profile,
+}
+
+final currentScreenProvider = StateProvider<MainScreen>((ref) => MainScreen.home);
+
+final onboardingCompleteProvider = StateProvider<bool>((ref) => false);
 
