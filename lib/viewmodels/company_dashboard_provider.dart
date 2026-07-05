32a7ffb80b6a8761108fb25be116b84059089d69
Anish-to <gautamanish1111111@gmail.com>
2026-07-05 11:23:52 +0545

    Update company_dashboard_provider.dart

--- lib/viewmodels/company_dashboard_provider.dart
+++ lib/viewmodels/company_dashboard_provider.dart
@@ -1,1 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+class CompanyDashboardState {
+  final int totalInternships;
+  final int activeInternships;
+  final int totalApplications;
+  final int hiredInterns;
+  final double conversionRate;
+  final int avgTimeToHire;
+
+  CompanyDashboardState({
+    this.totalInternships = 15,
+    this.activeInternships = 8,
+    this.totalApplications = 284,
+    this.hiredInterns = 42,
+    this.conversionRate = 14.8,
+    this.avgTimeToHire = 12,
+  });
+}
+
+class CompanyDashboardNotifier extends StateNotifier<CompanyDashboardState> {
+  CompanyDashboardNotifier() : super(CompanyDashboardState());
+
+  // In a real app, you would fetch this data from Firestore here
+  void refresh() {
+    // Fetch logic
+  }
+}
+
+final companyDashboardProvider = StateNotifierProvider<CompanyDashboardNotifier, CompanyDashboardState>((ref) {
+  return CompanyDashboardNotifier();
+});
 
